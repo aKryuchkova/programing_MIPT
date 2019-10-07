5d9a3f184df12bf0150e96cc29dbb521af9b62cd
nasty <nasty.kr2001@mail.ru>
2019-10-07 17:31:34 +0300

    homework

---
+++ aaaaaaaa.py
@@ -0,0 +1,16 @@
+import  math as math
+import turtle as tu
+tu.shape('turtle')
+
+
+
+def angel(n):
+    a = (n - 2) / n * 180
+    R = a/2/math.sin(math.pi/n)
+    b = math.sqrt((R ** 2 + R**2 - R*R* math.cos(a)))
+n = 3
+def(n)
+while n != 0:
+    tu.forward(a)
+    tu.left(180-a)
+    n -= 1
